36955ba91ce1324af4895beb6d502e9b749f15fc
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-03 00:04:59 +0530

    craazzyyyyist LIL-GUI

--- mesh.js
+++ mesh.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'lil-gui';
 
 let scene = new THREE.Scene();
 
@@ -73,9 +74,10 @@ scene.add(fillLightHelper);
 
 const rimLightHelper = new THREE.DirectionalLightHelper(rimLight, 2);
 scene.add(rimLightHelper);
+ 
 
 // Geometry - Cardboard Box
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const geometry = new THREE.BoxGeometry( 2, 2, 2,100,100 );
 const material = new THREE.MeshStandardMaterial( { 
   map: colorMap,
   normalMap: normalMap,
@@ -83,12 +85,120 @@ const material = new THREE.MeshStandardMaterial( {
   aoMap: aoMap,
   metalness: 0.0,
   roughness: 0.8,
+  displacementMap: heightMap,
+  displacementScale: 0.01,
   color: 0xcccccc // Fallback color
 } );
 const sphere = new THREE.Mesh( geometry, material );
 sphere.castShadow = true;
 sphere.receiveShadow = true;
 scene.add( sphere );
+
+// GUI Setup
+const gui = new GUI();
+
+// Material Settings
+const materialFolder = gui.addFolder('Material Settings');
+materialFolder.addColor(material, 'color').name('Color');
+materialFolder.add(material, 'metalness', 0, 1, 0.01).name('Metalness');
+materialFolder.add(material, 'roughness', 0, 1, 0.01).name('Roughness');
+materialFolder.add(material, 'displacementScale', 0, 0.1, 0.001).name('Displacement Scale');
+materialFolder.add(material, 'normalScale', { x: 0, y: 0 }, -2, 2, 0.1).name('Normal Scale');
+materialFolder.open();
+
+// Mesh Settings
+const meshFolder = gui.addFolder('Mesh Settings');
+meshFolder.add(sphere.scale, 'x', 0.1, 5, 0.1).name('Scale X');
+meshFolder.add(sphere.scale, 'y', 0.1, 5, 0.1).name('Scale Y');
+meshFolder.add(sphere.scale, 'z', 0.1, 5, 0.1).name('Scale Z');
+meshFolder.add(sphere.rotation, 'x', 0, Math.PI * 2, 0.01).name('Rotation X');
+meshFolder.add(sphere.rotation, 'y', 0, Math.PI * 2, 0.01).name('Rotation Y');
+meshFolder.add(sphere.rotation, 'z', 0, Math.PI * 2, 0.01).name('Rotation Z');
+meshFolder.add(sphere.position, 'x', -5, 5, 0.1).name('Position X');
+meshFolder.add(sphere.position, 'y', -5, 5, 0.1).name('Position Y');
+meshFolder.add(sphere.position, 'z', -5, 5, 0.1).name('Position Z');
+meshFolder.open();
+
+// Texture Settings
+const textureFolder = gui.addFolder('Texture Settings');
+const textureSettings = {
+  repeatX: 1,
+  repeatY: 1,
+  colorMapIntensity: 1,
+  normalMapIntensity: 1,
+  roughnessMapIntensity: 1,
+  aoMapIntensity: 1,
+  heightMapIntensity: 1
+};
+
+textureFolder.add(textureSettings, 'repeatX', 0.1, 10, 0.1).name('Repeat X').onChange(() => {
+  [colorMap, normalMap, roughnessMap, aoMap, heightMap].forEach(texture => {
+    texture.repeat.x = textureSettings.repeatX;
+    texture.needsUpdate = true;
+  });
+});
+
+textureFolder.add(textureSettings, 'repeatY', 0.1, 10, 0.1).name('Repeat Y').onChange(() => {
+  [colorMap, normalMap, roughnessMap, aoMap, heightMap].forEach(texture => {
+    texture.repeat.y = textureSettings.repeatY;
+    texture.needsUpdate = true;
+  });
+});
+
+textureFolder.add(textureSettings, 'colorMapIntensity', 0, 2, 0.1).name('Color Map Intensity').onChange(() => {
+  material.color.setHex(0xcccccc).multiplyScalar(textureSettings.colorMapIntensity);
+});
+
+textureFolder.add(textureSettings, 'normalMapIntensity', 0, 2, 0.1).name('Normal Map Intensity').onChange(() => {
+  material.normalScale.set(textureSettings.normalMapIntensity, textureSettings.normalMapIntensity);
+});
+
+textureFolder.add(textureSettings, 'roughnessMapIntensity', 0, 2, 0.1).name('Roughness Map Intensity').onChange(() => {
+  material.roughness = 0.8 * textureSettings.roughnessMapIntensity;
+});
+
+textureFolder.add(textureSettings, 'aoMapIntensity', 0, 2, 0.1).name('AO Map Intensity').onChange(() => {
+  material.aoMapIntensity = textureSettings.aoMapIntensity;
+});
+
+textureFolder.add(textureSettings, 'heightMapIntensity', 0, 0.1, 0.001).name('Height Map Intensity').onChange(() => {
+  material.displacementScale = textureSettings.heightMapIntensity;
+});
+
+textureFolder.open();
+
+// Geometry Settings
+const geometryFolder = gui.addFolder('Geometry Settings');
+const geometrySettings = {
+  width: 2,
+  height: 2,
+  depth: 2,
+  widthSegments: 100,
+  heightSegments: 100,
+  depthSegments: 1
+};
+
+geometryFolder.add(geometrySettings, 'width', 0.1, 5, 0.1).name('Width').onChange(updateGeometry);
+geometryFolder.add(geometrySettings, 'height', 0.1, 5, 0.1).name('Height').onChange(updateGeometry);
+geometryFolder.add(geometrySettings, 'depth', 0.1, 5, 0.1).name('Depth').onChange(updateGeometry);
+geometryFolder.add(geometrySettings, 'widthSegments', 1, 200, 1).name('Width Segments').onChange(updateGeometry);
+geometryFolder.add(geometrySettings, 'heightSegments', 1, 200, 1).name('Height Segments').onChange(updateGeometry);
+geometryFolder.add(geometrySettings, 'depthSegments', 1, 200, 1).name('Depth Segments').onChange(updateGeometry);
+
+function updateGeometry() {
+  const newGeometry = new THREE.BoxGeometry(
+    geometrySettings.width,
+    geometrySettings.height,
+    geometrySettings.depth,
+    geometrySettings.widthSegments,
+    geometrySettings.heightSegments,
+    geometrySettings.depthSegments
+  );
+  sphere.geometry.dispose();
+  sphere.geometry = newGeometry;
+}
+
+geometryFolder.open();
 
 window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
